f26daad994cab5769006de57357c760834c09125
Shivanand Patil <70444072+shivanand-patil@users.noreply.github.com>
2024-05-05 18:01:06 +0530

    validate.sh

---
+++ validate.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <config_file>"
+    exit 1
+fi
+
+python3 "$(dirname "$(realpath "$0")")/validate.py" "$1"
+
+if [ $? -ne 0 ]; then
+    echo "Structure not good, validate.py encountered errors. Exiting."
+    exit 1
+fi
+
+file_path="$1"
+
+version=$(asd --version | awk '{print $5}' )
+
+echo "Structure is good. Evaluating $file_path against version $version config schema."
+
+if asconfig validate --aerospike-version "$version" "$file_path"; then
+   	
+	echo "Validation successful for Aerospike version $version."
+else
+  	echo "Validation failed for Aerospike version $version"
+fi
